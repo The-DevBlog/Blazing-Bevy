8675668e26d376c9275a93ca020bc0d9932a577f
Andrew Smith <andrewcs149@gmail.com>
2021-07-31 10:12:20 -0700

    Delete WASM project

--- Blazor-Bevy/wwwroot/js/interop.js
+++
@@ -1,7 +0,0 @@
-function set(key, value) {
-    localStorage.setItem(key, value);
-}
-
-function get(key) {
-    return localStorage.getItem(key);
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-# Blazor-Bevy
-
-A Blazor WASM website that includes an in-browser 2D game created with the Bevy Game Engine written in Rust.
